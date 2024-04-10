818c1418d806dd8e8b648d1884279f93193b6e46
Johnson Kow <jkow95@gmail.com>
2024-04-10 12:46:00 -0400

    text fix

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -204,13 +204,14 @@ const HomePage = () => {
           to make your transition as easy as possible.
         </h1>
         <p className="text-xl">
-          If you're joining us in Berlin, we'll help with relocation and
-          paperwork. We’ll even provide you with free German or English lessons.
-          Plus, working in Germany means you can expect comprehensive health
-          insurance for you and your family, as well as generous maternity and
-          paternity leave. Office hours are flexible, but it’s not all work; we
-          have several company and team outings throughout the year as well as a
-          variety of fun, informal small-group activities.
+          If you&apos;re joining us in Berlin, we&apos;ll help with relocation
+          and paperwork. We&apos;ll even provide you with free German or English
+          lessons. Plus, working in Germany means you can expect comprehensive
+          health insurance for you and your family, as well as generous
+          maternity and paternity leave. Office hours are flexible, but
+          it&apos;s not all work; we have several company and team outings
+          throughout the year as well as a variety of fun, informal small-group
+          activities.
         </p>
       </section>
 
